88093588d93e4c348c226b9ff052e73e821d51e1
mooseph1 <mooseph@gmail.com>
2014-09-11 10:58:34 -0700

    my dice are plentiful

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,12 +1,28 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 int rollNumber;
-void setup()
+public void setup()
 {
 	size(1024,768);
 	background(0);
 	noLoop();
 }
 
-void draw()
+public void draw()
 {
 	background(0);
 	rollNumber = 0;
@@ -24,7 +40,7 @@ void draw()
 	text("Total = " + rollNumber, 512, 384);
 }
 
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -40,7 +56,7 @@ class Die
 		rollNumber = rollNumber + numDots;
 	}
 
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(dieX,dieY,8,8);
@@ -91,4 +107,13 @@ class Die
 		}
 		
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
